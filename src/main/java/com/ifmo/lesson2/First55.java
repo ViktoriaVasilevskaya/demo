711e4dfff95417697b788c05144c0c14e462595a
ViktoriaVasilevskaya <ya.vasilevskaya2012@yandex.ru>
2021-10-11 19:03:28 +0300

    Done

--- src/main/java/com/ifmo/lesson2/First55.java
+++ src/main/java/com/ifmo/lesson2/First55.java
@@ -5,6 +5,11 @@ public class First55 {
     Создайте программу, выводящую на экран первые 55 элементов последовательности 1 3 5 7 9 11 13 15 17 ….
      */
     public static void main(String[] args) {
-        // TODO implement
+        int x = 1;
+        System.out.println(x);
+        for (int i = 0; i < 54 ; i++) {
+            x += 2;
+            System.out.println(x);
+        }
     }
 }
