9d94b3d39cee5176f72ccdd3b11afc01c9932f2e
ViktoriaVasilevskaya <ya.vasilevskaya2012@yandex.ru>
2021-10-16 14:09:48 +0300

    Done

--- src/main/java/com/ifmo/lesson2/LuckyTickets.java
+++ src/main/java/com/ifmo/lesson2/LuckyTickets.java
@@ -15,8 +15,21 @@ public class LuckyTickets {
     }
 
     public static int luckyTickets() {
-        // TODO implement
+        int k = 0;
+        for (int i = 000001; i <= 999999 ; i++) {
+            int n1 = i / 100000;
+            int n2 = (i % 100000) /10000;
+            int n3 = (i % 10000) / 1000;
+            int n4 = (i % 1000) / 100;
+            int n5 = (i % 100) / 10;
+            int n6 = i % 10;
+            if(n1 + n2 + n3 == n4 + n5 + n6) {
+                k++;
+            }
 
-        return 0;
+        }
+
+        return k;
     }
+
 }
