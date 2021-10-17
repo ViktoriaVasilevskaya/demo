8b783d27d9b474d82808d1d62d169344d72e79f1
ViktoriaVasilevskaya <ya.vasilevskaya2012@yandex.ru>
2021-10-17 22:59:37 +0300

    Done

---
+++ src/main/java/com/ifmo/lesson3/Random12.java
@@ -0,0 +1,50 @@
+package com.ifmo.lesson3;
+import java.util.Arrays;
+import java.util.Random;
+public class Random12 {
+    /*
+    Создайте массив из 12 случайных целых чисел из отрезка [-15;15]. Определите какой
+    элемент является в этом массиве максимальным и сообщите индекс его последнего
+    вхождения в массив.
+     */
+    public static void main(String[] args) {
+        int[] randomNumbers = randomNumbers();
+
+        System.out.println(Arrays.toString(randomNumbers));
+
+        int max = max(randomNumbers);
+        int maxLastIndex = lastIndexOf(randomNumbers, max);
+
+        System.out.println(max);
+        System.out.println(maxLastIndex);
+
+    }
+
+    public static int[] randomNumbers() {
+        int [] arr = new int [12];
+        Random random = new Random ();
+        for (int i = 0; i < arr.length ; i++) {
+            arr [i] = random.nextInt(30)-15;
+        }
+        return arr;
+    }
+
+    public static int max(int[] randomNumbers) {
+        int max = randomNumbers[0];
+        for (int i:randomNumbers) {
+            if (i > max){
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    public static int lastIndexOf(int[] randomNumbers, int max) {
+        for (int i = 0; i < randomNumbers.length; i++) {
+            if(randomNumbers [i] == max){
+                return i;
+            }
+        }
+        return max;
+    }
+}
