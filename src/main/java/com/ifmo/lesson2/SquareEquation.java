02249ca9875bf66b14939568e167d3ce2b5451d9
ViktoriaVasilevskaya <ya.vasilevskaya2012@yandex.ru>
2021-10-17 19:54:41 +0300

    Done

--- src/main/java/com/ifmo/lesson2/SquareEquation.java
+++ src/main/java/com/ifmo/lesson2/SquareEquation.java
@@ -22,8 +22,15 @@ public class SquareEquation {
     Возвращает массив из двух корней или null, если таковых нет.
      */
     public static double[] squareEquationRoots(double a, double b, double c) {
-        // TODO implement
+        double[] arr = new double [2];
+        double d = b * b - 4 * a * c;
+        if (d >= 0) {
+            arr [0] = (-b - Math.sqrt(d)) / 2 / a;
+            arr [1] = (-b + Math.sqrt(d)) / 2 / a;
+            return arr;
+        } else {
+            return null;
+        }
 
-        return null;
     }
 }
